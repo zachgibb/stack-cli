da5e4c7276b1dddd30536f8d0323b796a4702518
Zach Gibb <zachgibb@gmail.com>
2014-08-25 19:06:47 -0400

    shows the body of the question chosen

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var SE = require('stackexchange');
-var entities = require('entities');
 var prompt = require('prompt');
+var htmlToText = require('html-to-text');
 
 var context = new SE({ 
   clientId: 3502,
@@ -11,7 +11,7 @@ var context = new SE({
 
 
 var stack = function() {
-
+  console.log(context);
   var criteria = {
     key: 'zUBS9XBccJH*ZodpviQbgw((',
     pagesize: 10,
@@ -30,6 +30,7 @@ var stack = function() {
     // url:'',
     // views:'',
     // wiki:'',
+    // filter: '!bA1coBfqSKVk-5'
   };
   context.search.advanced(criteria, function(err, answers){
     if (err) throw err;
@@ -41,18 +42,30 @@ var stack = function() {
       prompt.start();
 
       answers.items.forEach(function(item, index){
-        console.log('\n['+index+']: ' + entities.decodeHTML(item.title));
+        console.log('\n['+index+']: ' + htmlToText.fromString(item.title));
       });
 
-      var message = 'Which answers would you like to see?';
+      var message = 'Which post would you like to see?';
 
       prompt.get(message, function(err, result) {
+        var question = answers.items[result[message]];
         if (err) throw err;
-        console.log(
-          answers.items[
-            result[message]
-          ]
-        );
+
+
+        context.questions.questions({
+          key: 'zUBS9XBccJH*ZodpviQbgw((',
+          sort: 'activity',
+          order: 'asc',
+          filter: '!bA1coBfqSKVk-5'
+        }, function(err, result){
+
+          console.log('\x1B[1m\x1B[4m'+htmlToText.fromString(question.title)+'\x1B[24m\x1B[22m');
+          console.log('\n'+htmlToText.fromString(result.items[0].body));
+          console.log('\n-- '+question.owner.display_name);
+
+
+        }, [question.question_id]);
+
       });
 
     }
