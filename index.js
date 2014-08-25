4c381884ef4ffe408caee154c7a8b248ab8e7e09
Zach Gibb <zachgibb@gmail.com>
2014-08-25 17:51:03 -0400

    allowed for searching of multiple stack exchange sites

--- index.js
+++ index.js
@@ -3,49 +3,19 @@ var entities = require("entities");
 
 var context = new SE({ 
   clientId: 3502,
-  version: 2.2 
+  version: 2.2,
+  site: process.argv[3] || 'stackoverflow', 
 });
 
-var siteFlags = {
-  '-stackoverflow': 'stackoverflow',
-  '-serverfault': 'serverfault',
-  '-superuser': 'superuser',
-  '-meta': 'meta',
-  '-webapps': 'webapps',
-  '-meta.webapps': 'meta.webapps',
-  '-gaming': 'gaming',
-  '-meta.gaming': 'meta.gaming',
-  '-webmasters': 'webmasters',
-  '-meta.webmasters': 'meta.webmasters',
-  '-cooking': 'cooking',
-  '-meta.cooking': 'meta.cooking',
-  '-gamedev': 'gamedev',
-  '-meta.gamedev': 'meta.gamedev',
-  '-photo': 'photo',
-  '-meta.photo': 'meta.photo',
-  '-stats': 'stats',
-  '-meta.stats': 'meta.stats',
-  '-math': 'math',
-  '-meta.math': 'meta.math',
-  '-diy': 'diy',
-  '-meta.diy': 'meta.diy',
-  '-meta.superuser': 'meta.superuser',
-  '-meta.serverfault': 'meta.serverfault',
-  '-gis': 'gis',
-  '-meta.gis': 'meta.gis',
-  '-tex': 'tex',
-  '-meta.tex': 'meta.tex',
-  '-askubuntu': 'askubuntu',
-  '-meta.askubuntu': 'meta.askubuntu'
-};
+
 
 var stack = function() {
+
   var criteria = {
     key: 'zUBS9XBccJH*ZodpviQbgw((',
     pagesize: 10,
     sort: 'relevance',
     order: 'asc',
-    site: 'stackoverflow',
     q: process.argv[2],
     // accepted:'',
     // answers:'',
