9c8952322833909f7b4ddcbfd3e815532754e694
Zach Gibb <zachgibb@gmail.com>
2014-08-25 17:28:37 -0400

    search now prints list of readable relevant titles

--- index.js
+++ index.js
@@ -1,35 +1,75 @@
-// argv
-  // 0 stack
-  // 1 question
-  // 2 site
-
 var SE = require('stackexchange');
+var entities = require("entities");
 
 var context = new SE({ 
   clientId: 3502,
   version: 2.2 
 });
 
-var filter = {
-  key: 'zUBS9XBccJH*ZodpviQbgw((',
-  pagesize: 10,
-  tagged: 'node.js',
-  sort: 'activity',
-  order: 'asc'
+var siteFlags = {
+  '-stackoverflow': 'stackoverflow',
+  '-serverfault': 'serverfault',
+  '-superuser': 'superuser',
+  '-meta': 'meta',
+  '-webapps': 'webapps',
+  '-meta.webapps': 'meta.webapps',
+  '-gaming': 'gaming',
+  '-meta.gaming': 'meta.gaming',
+  '-webmasters': 'webmasters',
+  '-meta.webmasters': 'meta.webmasters',
+  '-cooking': 'cooking',
+  '-meta.cooking': 'meta.cooking',
+  '-gamedev': 'gamedev',
+  '-meta.gamedev': 'meta.gamedev',
+  '-photo': 'photo',
+  '-meta.photo': 'meta.photo',
+  '-stats': 'stats',
+  '-meta.stats': 'meta.stats',
+  '-math': 'math',
+  '-meta.math': 'meta.math',
+  '-diy': 'diy',
+  '-meta.diy': 'meta.diy',
+  '-meta.superuser': 'meta.superuser',
+  '-meta.serverfault': 'meta.serverfault',
+  '-gis': 'gis',
+  '-meta.gis': 'meta.gis',
+  '-tex': 'tex',
+  '-meta.tex': 'meta.tex',
+  '-askubuntu': 'askubuntu',
+  '-meta.askubuntu': 'meta.askubuntu'
 };
 
-context.questions.questions(filter, function(err, results){
-  if (err) throw err;
-  for (var i = 0; i < results.items.length; i++) {
-    console.log('\n['+i+']: ' + results.items[i].title);
-  }
-  console.log(results.has_more, results.quota_remaining);
-});
-
 var stack = function() {
-
+  var criteria = {
+    key: 'zUBS9XBccJH*ZodpviQbgw((',
+    pagesize: 10,
+    sort: 'relevance',
+    order: 'asc',
+    site: 'stackoverflow',
+    q: process.argv[2],
+    // accepted:'',
+    // answers:'',
+    // body:'',
+    // closed:'',
+    // migrated:'',
+    // notice:'',
+    // nottagged:'',
+    // title:'',
+    // user:'',
+    // url:'',
+    // views:'',
+    // wiki:'',
+  };
+  context.search.advanced(criteria, function(err, results){
+    if (err) throw err;
+    results.items.forEach(function(item, index){
+      console.log('\n['+index+']: ' + entities.decodeHTML(item.title));
+    });
+  });
 };
 
 module.exports = {
   stack: stack,
 };
+
+stack();
