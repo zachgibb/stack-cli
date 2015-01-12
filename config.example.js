5b4ef7f9ac8cc12d0f5f10cb4b7c68f219fada0f
Zach Gibb <zachgibb@gmail.com>
2015-01-12 16:04:52 -0500

    Fix punctuation

--- config.example.js
+++ config.example.js
@@ -2,7 +2,7 @@
 //set options and then save config.js in main directory
 //note, this isn't required and one will be created for you when 
 //you save in the app
-//To be able to log into your account. go to the below link and authorize this app
+//To be able to log into your account. Go to the below link and authorize this app.
 
 
 //https://stackexchange.com/oauth/dialog?&client_id=3513&scope=read_inbox%20write_access%20no_expiry%20private_info&redirect_uri=https://stackexchange.com/oauth/login_success
